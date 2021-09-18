207d159dec03666b895da6191acdf3395515cdb0
NguyenThaiHoc1 <thaihocmap123@gmail.com>
2021-09-18 17:42:47 +0700

    Create Models and Urls direction and register Models to admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 
+from .models import Book, Genre, BookInstance, Language
 # Register your models here.
 from .models import Question
 
-admin.site.register(Question) # đăng ký bảng question lên admin để quản lý
+admin.site.register(Question)  # đăng ký bảng question lên admin để quản lý
+
+# admin.site.register(Book)
+
+admin.site.register(Genre)
+
+admin.site.register(BookInstance)
+
+admin.site.register(Language)
+
+
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_date')
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.urls import include
+from django.urls import path
+from django.views.generic import RedirectView
+
+
+# cai này trước nghe bảo là ko nên
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +29,7 @@ urlpatterns = [
 
 urlpatterns += [
     path('catalog/', include('catalog.urls')),
+    path('', RedirectView.as_view(url='catalog/', permanent=True))  # dùng để redirect view
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
