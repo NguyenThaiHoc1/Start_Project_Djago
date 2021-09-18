6945e70aae97333d1baa36267061e118b00b5a94
NguyenThaiHoc1 <thaihocmap123@gmail.com>
2021-09-18 09:56:08 +0700

    Creating dicrect path when user sent request

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,31 @@
+from django.http import Http404
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Question
+
+
 # Create your views here.
+def index(request):
+    lastest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {
+        'latest_question_list': lastest_question_list,
+    }
+    return render(request, template_name='catalog/index.html', context=context)
+
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question doesn't exists")
+    return render(request, template_name='catalog/detail.html', context={'question': question})
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
